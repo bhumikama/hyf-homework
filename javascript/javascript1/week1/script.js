48c9f619e7a8347455c2af7ad645274bee289df9
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2024-06-03 11:10:43 +0200

    Implemented house price estimator

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -18,3 +18,37 @@ if (shouldShowResultInDogYears) {
     `Your dog will be ${dogYear} human years old in ${dogYearFuture}`
   );
 }
+
+//Housey pricey (A house price estimator)
+
+/*
+Find the volume using below formula
+Volume=Width×Height×Depth
+*/
+
+function findVolume(width, height, depth) {
+  return width * height * depth;
+}
+
+function findHousePrice(volumeInMeters, gardenSize) {
+  return volumeInMeters * 2.5 * 1000 + gardenSize * 300;
+}
+
+function compare(estimatedPrice, costPrice) {
+  if (estimatedPrice > costPrice) {
+    console.log(`paying too little. The estimated price is:${estimatedPrice}`);
+  } else {
+    console.log(
+      `paying more than the estimated cost. The estimated price is:${estimatedPrice}`
+    );
+  }
+}
+
+const volumeInMeters1 = findVolume(8, 10, 10); //Peter
+const volumeInMeters2 = findVolume(5, 8, 11); //Julia
+
+const estimatedPricePeter = findHousePrice(volumeInMeters1, 100);
+const estimatedPriceJulia = findHousePrice(volumeInMeters2, 70);
+
+const comparePricePeter = compare(estimatedPricePeter, 2500000);
+const comparePriceJulia = compare(estimatedPriceJulia, 1000000);
