5f330a2f276858fbc906c556a45f99a52ada3821
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2024-06-03 11:06:23 +0200

    Implemented age-ify calculator

---
+++ javascript/javascript1/week1/script.js
@@ -0,0 +1,6 @@
+//Age-ify (A future age calculator)
+
+const yearOfBirth = 1997;
+const yearFuture = 2055;
+const age = yearFuture - yearOfBirth;
+console.log(`You will be ${age} years old in ${yearFuture}.`);
