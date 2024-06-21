69eeaa0465052f593c20a56b3e33501e5d64077f
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2024-06-21 11:31:35 +0200

    Implemented Item Array Removal

---
+++ javascript/javascript1/week3/solution.js
@@ -0,0 +1,21 @@
+//Item Array Removal
+
+const names = [
+  "Peter",
+  "Ahmad",
+  "Yana",
+  "kristina",
+  "Rasmus",
+  "Samuel",
+  "katrine",
+  "Tala",
+];
+const nameToRemove = "Ahmad";
+
+for (let i = 0; i < names.length; i++) {
+  if (names[i] === nameToRemove) {
+    names.splice(i, 1); // i represents the index where specified name is present and 1 (number of elements to remove)
+  }
+}
+
+console.log(names);
