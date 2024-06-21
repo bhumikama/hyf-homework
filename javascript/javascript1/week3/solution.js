623cab2c98bc48957fbe9e7071b69aab8d45b51a
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2024-06-21 11:34:10 +0200

    Implemented when we will be there task

--- javascript/javascript1/week3/solution.js
+++ javascript/javascript1/week3/solution.js
@@ -19,3 +19,31 @@ for (let i = 0; i < names.length; i++) {
 }
 
 console.log(names);
+
+//When we will be there
+function timeTakenToReturn(travelInformation) {
+  if (
+    typeof travelInformation.destinationDistance === "number" &&
+    typeof travelInformation.speed === "number"
+  ) {
+    const timeTaken =
+      travelInformation.destinationDistance / travelInformation.speed;
+    const timeTakenInHours = Math.floor(timeTaken);
+    const timeTakenInMinutes = Math.floor((timeTaken - timeTakenInHours) * 60);
+    return `${timeTakenInHours} and ${timeTakenInMinutes} minutes`;
+  } else {
+    return "cannot find the time taken for travel as the speed and distance are not numbers";
+  }
+}
+
+function padWithZero(time) {
+  return time < 10 ? "0" + time : time;
+}
+
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+const travelTime = timeTakenToReturn(travelInformation);
+console.log(travelTime);
