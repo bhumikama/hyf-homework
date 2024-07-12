078d43585eb91c5bd225328ee511acb2c2b5699a
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2024-07-12 12:34:56 +0200

    added sorting function for hyfBay

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -36,11 +36,31 @@ function filterProducts() {
   renderProducts(filteredProducts);
 }
 
+function sortProducts(products, sortType) {
+  const sortedProducts = [...products];
+  sortedProducts.sort((a, b) => {
+    if (sortType === "price") {
+      return a.price - b.price;
+    } else if (sortType === "name") {
+      return a.name.localeCompare(b.name);
+    } else if (sortType === "rating") {
+      return (b.rating || 0) - (a.rating || 0);
+    }
+    return 0;
+  });
+  renderProducts(sortedProducts);
+}
+
 document
   .querySelector("#search-input")
   .addEventListener("input", filterProducts);
 document
   .querySelector("#max-price-input")
   .addEventListener("input", filterProducts);
+
+document.querySelector("#sort-select").addEventListener("change", (event) => {
+  const sortType = event.target.value;
+  sortProducts(products, sortType);
+});
 
 renderProducts(products);
