c996fc5508efbb021178e210eaa3e67292ae8e97
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2024-10-29 13:56:19 +0100

    created a resposive navbar and created appropriate routes

---
+++ react/react2/week1/week1-homework/app/astronomy/page.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default async function AstronomyPic() {
+  const API_KEY = "EeSfTBChJ8VYja2AH2LetV3iDAGAaAd7hT14qCfZ";
+
+  const apiResponse = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`,
+    { cache: "no-store" }
+  );
+  if (!apiResponse.ok) {
+    throw new Error("Astronomy picture not found");
+  }
+  const astronomyPic = await apiResponse.json();
+  console.log(astronomyPic);
+  return (
+    <div className="min-h-screen w-full">
+      {astronomyPic ? (
+        <div className="flex flex-col items-center justify-center gap-5 py-5">
+          <img
+            className="border-2 h-96 w-96"
+            src={astronomyPic.url}
+            alt={astronomyPic.title}
+          ></img>
+          <div className="max-w-2xl bg-[#262b40] py-5 px-6 mx-3">
+            <p className="text-md mx-auto">{astronomyPic.explanation}</p>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p>Loading the picture....</p>
+        </div>
+      )}
+    </div>
+  );
+}
