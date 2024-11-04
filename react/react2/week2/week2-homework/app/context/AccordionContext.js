6aba27894e7a0c062351d2ff7682f598c23c0126
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2024-11-04 09:09:51 +0100

    handled onChange in task 2

--- react/react2/week2/week2-homework/app/context/AccordionContext.js
+++ react/react2/week2/week2-homework/app/context/AccordionContext.js
@@ -4,11 +4,11 @@ import { createContext, useState } from "react";
 export const AccordionContext = createContext();
 
 export const AccordionProvider = ({ children }) => {
-  //created a global state variable "activeIndex" so that all the components of the tree are aware of the activePanel  
+  //created a global state variable "activeIndex" so that all the components of the tree are aware of the activePanel
   const [activeIndex, setActiveIndex] = useState(0);
-  //function to change active index when the button is clicked
+  //function to change active index when the button is clicked, if the button was already clicked then activeIndex is set to null to handle the toggling of displaying the content
   const onOpen = (index) => {
-    setActiveIndex(index);
+    setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
   };
   return (
     <AccordionContext.Provider value={{ activeIndex, onOpen }}>
